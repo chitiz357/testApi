78b6b4609fdc36180b3449a565181e289f921411
bryan <thenhernandezb@gmail.com>
2021-08-14 20:49:04 -0400

    cosa

--- dist/index.js
+++ dist/index.js
@@ -6,6 +6,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const app = express_1.default();
 app.get('/', (req, res) => {
-    res.send('hola mundo!');
+    res.send('hola mundoo!');
 });
-app.listen(3001, () => console.log('runnig'));
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, () => console.log(`runnig in port ${PORT}`));
