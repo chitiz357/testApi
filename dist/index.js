b6c2166e7f82a75331a9709f1f6f9f75ec0938fa
bryan <thenhernandezb@gmail.com>
2021-08-14 20:53:21 -0400

    install: cors

--- dist/index.js
+++ dist/index.js
@@ -4,7 +4,9 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const cors_1 = __importDefault(require("cors"));
 const app = express_1.default();
+app.use(cors_1.default);
 app.get('/', (req, res) => {
     res.send('hola mundoo!');
 });
--- index.ts
+++ index.ts
@@ -1,6 +1,9 @@
 import  Express  from "express";
+import cors from 'cors'
 
 const app = Express()
+
+app.use(cors)
 
 app.get('/',(req,res)=> {
 res.send('hola mundoo!')
