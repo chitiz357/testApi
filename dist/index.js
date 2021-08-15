875fe488b8b9dd0b663acf0c065ad987b2c51b79
bryan <thenhernandezb@gmail.com>
2021-08-14 20:15:34 -0400

    add: adding npm run start

--- dist/index.js
+++ dist/index.js
@@ -1,1 +1,11 @@
-console.log('hola');
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const app = express_1.default();
+app.get('/', (req, res) => {
+    res.send('hola mundo!');
+});
+app.listen(3001, () => console.log('runnig'));
