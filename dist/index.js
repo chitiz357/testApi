25358ce1a7f4895ab39d08e7e3f5f593234c7f06
bryan <thenhernandezb@gmail.com>
2021-08-16 02:09:42 -0400

    fix: reincorporate cors

--- dist/index.js
+++ dist/index.js
@@ -4,9 +4,11 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const cors_1 = __importDefault(require("cors"));
 const app = express_1.default();
+app.use(cors_1.default());
 app.get('/', (request, response) => {
-    response.send('<h1>Hello Worldaaaaaaaaaaaa!</h1>');
+    response.send('<h1>Hello World!</h1>');
 });
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => console.log(`runnig in port!!!! ${PORT}`));
--- index.ts
+++ index.ts
@@ -3,10 +3,12 @@ import cors from "cors";
 
 const app = Express();
 
+app.use(cors())
+
 
 
 app.get('/', (request, response) => {
-    response.send('<h1>Hello Worldaaaaaaaaaaaa!</h1>')
+    response.send('<h1>Hello World!</h1>')
   })
 
 const PORT = process.env.PORT || 3001;
