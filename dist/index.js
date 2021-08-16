c938c826e2f79fcc7c0501e54cc90d2d99a248a2
bryan <thenhernandezb@gmail.com>
2021-08-16 01:56:40 -0400

    fix: server is running. cors causing problem

--- dist/index.js
+++ dist/index.js
@@ -4,11 +4,9 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
-const cors_1 = __importDefault(require("cors"));
 const app = express_1.default();
-app.use(cors_1.default);
-app.get('/', (req, res) => {
-    res.send('hola mundoo!');
+app.get('/', (request, response) => {
+    response.send('<h1>Hello Worldaaaaaaaaaaaa!</h1>');
 });
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => console.log(`runnig in port!!!! ${PORT}`));
--- index.ts
+++ index.ts
@@ -1,14 +1,14 @@
-import  Express  from "express";
-import cors from 'cors'
+import Express from "express";
+import cors from "cors";
 
-const app = Express()
+const app = Express();
 
-app.use(cors)
 
-app.get('/',(req,res)=> {
-res.send('hola mundoo!')
-})
 
-const PORT = process.env.PORT || 3001
+app.get('/', (request, response) => {
+    response.send('<h1>Hello Worldaaaaaaaaaaaa!</h1>')
+  })
 
-app.listen(PORT,()=>console.log(`runnig in port!!!! ${PORT}`))
+const PORT = process.env.PORT || 3001;
+
+app.listen(PORT, () => console.log(`runnig in port!!!! ${PORT}`));
